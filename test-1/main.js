1a0132eb51284b3c19cffe73a8b2382c63dacfaa
rerdos <ricsi96@gmail.com>
2015-01-19 22:08:17 +0100

    Random number generator

--- test-1/main.js
+++ test-1/main.js
@@ -5,6 +5,18 @@ angular.module('myApp', [])
 
     .controller('MainCtrl', function($scope){
 
+        $scope.randNumber = function(min, max){
+            if(min > max){
+                console.error("Error (function:randNumber): The first parameter (min) must be less than the second parameter (max).")
+                return;
+            }
+            var originNumber = Math.random();
+            var calcMax = max - min;
+            var randFloatNumber = min + calcMax * originNumber;
+            var randNumber = Math.round(randFloatNumber);
+            return randNumber;
+        };
+
         $scope.percent = 0;
 
 
